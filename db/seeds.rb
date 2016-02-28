bf713531aec6d2fdacd44191ee4c18473c0a8c9c
hashmee <hashmee.smf@gmail.com>
2016-02-28 18:25:15 +0530

    added schema for user table and fetch images throgh polymorphic associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name: 'user1')
+User.create(name: 'user2')
+User.create(name: 'user3')
+Image.create(name: 'image1', imageable: User.first)
+Image.create(name: 'image2', imageable: User.first)
+Image.create(name: 'image3', imageable: User.first)
+Image.create(name: 'image4', imageable: User.second)
+Image.create(name: 'image5', imageable: User.second)
+Image.create(name: 'image6', imageable: User.third)
+Image.create(name: 'image7', imageable: User.third)
+Image.create(name: 'image8', imageable: User.third)
+Image.create(name: 'image9', imageable: User.third)
+Image.create(name: 'image10', imageable: User.third)
